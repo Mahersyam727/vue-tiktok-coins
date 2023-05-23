fb0085110a1474693a186e5a232c0d8d1fe0ca9b
ptrv-dev <petrov99119@gmail.com>
2023-05-23 17:42:50 +0300

    AppHeader

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+export default {
+  components: { AppHeader },
+};
+</script>
+
 <template>
+  <app-header />
   <h1>App</h1>
 </template>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<script>
+import AppLogo from './AppLogo.vue';
+import AppSearch from './AppSearch.vue';
+import AppActions from './AppActions.vue';
+
+export default {
+  name: 'Header',
+  components: { AppLogo, AppSearch, AppActions },
+};
+</script>
+
+<template>
+  <div class="tiktok-xk7ai4-DivHeaderContainer e10win0d0">
+    <div class="tiktok-notu47-DivHeaderWrapperMain e10win0d1">
+      <!-- Logo -->
+      <app-logo />
+      <!-- Search -->
+      <app-search />
+      <!-- Actions -->
+      <app-actions />
+    </div>
+  </div>
+</template>
