2a7df324b8d9e8e675651fffde53fd9e1b13c966
ptrv-dev <petrov99119@gmail.com>
2023-05-23 17:42:24 +0300

    AppSearch

---
+++ src/components/AppSearch.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="tiktok-1x100u9-DivHeaderCenterContainer e10c4g870">
+    <div class="tiktok-thiumz-DivSearchFormContainer eagc08l0">
+      <form
+        class="search-input tiktok-dhqzc6-FormElement ev30f210"
+        action="/search"
+      >
+        <input
+          placeholder="Поиск"
+          name="q"
+          type="search"
+          autocomplete="off"
+          role="combobox"
+          aria-controls=""
+          aria-label="Поиск"
+          aria-expanded="false"
+          aria-autocomplete="list"
+          data-e2e="search-user-input"
+          class="tiktok-fkcqlv-InputElement ev30f212"
+          value=""
+        /><span class="tiktok-hck1rr-SpanSpliter ev30f215"></span
+        ><button
+          type="submit"
+          aria-label="Поиск"
+          data-e2e="search-button"
+          class="tiktok-laero7-ButtonSearch ev30f216"
+        >
+          <div class="tiktok-17iic05-DivSearchIconContainer ev30f217">
+            <svg
+              width="24"
+              data-e2e=""
+              height="24"
+              viewBox="0 0 48 48"
+              fill="rgba(22, 24, 35, .34)"
+              xmlns="http://www.w3.org/2000/svg"
+              data-darkreader-inline-fill=""
+              style="--darkreader-inline-fill: rgba(214, 210, 205, 0.34)"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M22 10C15.3726 10 10 15.3726 10 22C10 28.6274 15.3726 34 22 34C28.6274 34 34 28.6274 34 22C34 15.3726 28.6274 10 22 10ZM6 22C6 13.1634 13.1634 6 22 6C30.8366 6 38 13.1634 38 22C38 25.6974 36.7458 29.1019 34.6397 31.8113L43.3809 40.5565C43.7712 40.947 43.7712 41.5801 43.3807 41.9705L41.9665 43.3847C41.5759 43.7753 40.9426 43.7752 40.5521 43.3846L31.8113 34.6397C29.1019 36.7458 25.6974 38 22 38C13.1634 38 6 30.8366 6 22Z"
+              ></path>
+            </svg>
+          </div>
+        </button>
+        <div class="tiktok-18w6lxc-DivInputBorder ev30f211"></div>
+      </form>
+    </div>
+  </div>
+</template>
