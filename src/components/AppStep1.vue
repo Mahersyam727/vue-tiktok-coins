a18d549da39556213921f49ec0e9d471d74dbaf2
ptrv-dev <petrov99119@gmail.com>
2023-05-24 12:03:06 +0300

    AppSteps components

---
+++ src/components/AppStep1.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container" id="step1">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      width="50px"
+      height="50px"
+      viewBox="0 0 100 100"
+      preserveAspectRatio="xMidYMid"
+    >
+      <circle
+        cx="50"
+        cy="50"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="5"
+        r="35"
+        stroke-dasharray="164.93361431346415 56.97787143782138"
+      >
+        <animateTransform
+          attributeName="transform"
+          type="rotate"
+          repeatCount="indefinite"
+          dur="1s"
+          values="0 50 50;360 50 50"
+          keyTimes="0;1"
+        ></animateTransform>
+      </circle>
+    </svg>
+    <h3>Processing your payment</h3>
+    <p>This could take a few seconds</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  svg {
+    margin-bottom: 0.5rem;
+  }
+  h3 {
+    font-size: 1.125rem;
+    padding: 0;
+    margin: 0;
+  }
+  p {
+    font-size: 1rem;
+    padding: 0;
+    margin: 0;
+  }
+}
+</style>
---
+++ src/components/AppStep3.vue
@@ -0,0 +1,53 @@
+<script>
+export default {
+  props: {
+    price: {
+      type: Number,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="container" id="step3" @click.stop="">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="#00C568"
+      width="48"
+      height="48"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+      />
+    </svg>
+    <h3>Payment completed</h3>
+    <p>
+      You recharged
+      <strong>{{
+        price.toLocaleString('en', { style: 'currency', currency: 'EUR' })
+      }}</strong>
+      to Client Account<br />
+      You can use coins to send virtual Gifts
+    </p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  h3 {
+    font-size: 18px;
+    margin-top: 1rem;
+  }
+  p {
+    margin-top: 0.25rem;
+    font-size: 1rem;
+    text-align: center;
+  }
+}
+</style>
